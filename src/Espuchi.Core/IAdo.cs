05c4a5a157e5b94dfca00f36109b5bfb0fd4e45d
supantajose <jose.supantaet12d1@gmail.com>
2023-11-10 09:08:47 -0300

    LISTO

--- src/Espuchi.Core/IAdo.cs
+++ src/Espuchi.Core/IAdo.cs
@@ -6,11 +6,6 @@ public interface IAdo
 {
     void altaBanda (Banda banda);
     List<Banda> ObtenerBandas();
-    void altaNombre(nombre nombre);
-    List<Nombre> ObtenerNombre();
-    void altaAnio (Anio anio);
-    List<Banda> ObtenerAnio();
-    void altaBanda(Banda banda, string pass);
     Banda? ObtenerBanda(string nombre, ushort anio);
 
 }
