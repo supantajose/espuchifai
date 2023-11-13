acb4c974ed10aa408714d859e82d48ef6aaf649b
supantajose <jose.supantaet12d1@gmail.com>
2023-11-13 10:39:54 -0300

    listo

--- src/Espuchi.Core/IAdo.cs
+++ src/Espuchi.Core/IAdo.cs
@@ -6,6 +6,11 @@ public interface IAdo
 {
     void altaBanda (Banda banda);
     List<Banda> ObtenerBandas();
-    Banda? ObtenerBanda(string nombre, ushort anio);
-
+    Banda? ObtenerBanda(int id_banda, string nombre, ushort anio);
+    void altaAlbumes (Albumes albumes);
+    List<Albumes> ObtenerAlbumes();
+    Albumes? ObtenerAlbumes(int id_album, string nombre, DateTime lanzamiento, int id_banda,int Reproduccion);
+    void altaCanciones (Canciones canciones);
+    List<Canciones> ObtenerCanciones();
+    Canciones? ObtenerCanciones(int idcancion, string nombre, int numero, int id_album, int Reproduccion); 
 }
