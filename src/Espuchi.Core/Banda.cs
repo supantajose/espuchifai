0b8468885baaedc69841e31b490b971f23b311c3
supantajose <jose.supantaet12d1@gmail.com>
2023-11-29 13:15:11 -0300

    listo

--- src/Espuchi.Core/Banda.cs
+++ src/Espuchi.Core/Banda.cs
@@ -4,6 +4,7 @@ public class Banda
     public int id_banda {get; set; }
     public required string Nombre {get; set;} 
     public DateTime anio {get; set;}
-    public required List<Albumes> Albumes {get; set;}
+    public List<Albumes> Albumes {get; set;}
+        = new List<Albumes>();
     
 }
